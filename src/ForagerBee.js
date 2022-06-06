3a3ac41d88ca1f1c7bd82a0ff28b17b4af2b3a9c
Marshall Zmuda <marshall.z@outlook.com>
2022-06-06 12:02:38 -0700

    Implement 6ees folder with ES6 Instatiation

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
-};
+  constructor(age) {
+    super(age || 10);
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage() {
+    this.treasureChest.push('treasure');
+  }
+}
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,11 @@
 class Grub {
-};
+  constructor(age) {
+    this.age = age || 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+}
